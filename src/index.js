270efb9971d26fa2bb92d02a873f4eae77417160
Christyan Melo <christyanmelo@MacBook-Pro-de-Christyan.local>
2026-06-30 18:49:16 -0300

    fix: Express serve admin.html at / em vez de redirect /admin (quebrava o proxy Vercel)

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ app.get('/r/:token', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.redirect('/admin')
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'))
 })
 
 app.get('/admin', (req, res) => {
